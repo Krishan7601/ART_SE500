6c7ca38eceea0648662b76fb2826be0399f2615c
Krishan7601 <Krishan7603@gmail.com>
2016-09-25 20:55:15 -0400

    Added files via drag and drop
    Contains UML and code chooser classes, need to link imported files to "parser" classes

---
+++ ART/src/edu/erau/se500/art/UMLChooser.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import javafx.application.Application;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public final class UMLChooser extends Application
+{
+	public static void main(String[] args) 
+	{
+		Application.launch(args);
+	}
+
+	public void start(final Stage umlChooserStage)
+	{
+		umlChooserStage.setTitle("UML Requirement Chooser");
+
+		final FileChooser umlChooser = new FileChooser();
+		FileChooser.ExtensionFilter extFilter = 
+				new FileChooser.ExtensionFilter("XMI files (*.XMI)", "*.XMI");
+		umlChooser.getExtensionFilters().add(extFilter);
+		@SuppressWarnings("unused")
+		File umlRequirementFile = umlChooser.showOpenDialog(umlChooserStage);
+		
+		//save file
+	}
+}
+
+
