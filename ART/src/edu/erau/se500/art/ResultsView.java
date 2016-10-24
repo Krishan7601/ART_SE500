359cee9dd7edbbbb03135a3d6571659b22971f3f
Kevin <kevinric7@hotmail.com>
2016-10-24 09:34:55 -0400

    results table with filler data

--- ART/src/edu/erau/se500/art/ResultsView.java
+++ ART/src/edu/erau/se500/art/ResultsView.java
@@ -2,9 +2,8 @@ package edu.erau.se500.art;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.ui.part.ViewPart;
 
@@ -18,16 +17,24 @@ public class ResultsView extends ViewPart {
 		
 		this.mainPanel = mainPanel;
 		
-		  final Table table = new Table (mainPanel, SWT.VIRTUAL | SWT.BORDER);
-		  table.setItemCount (1000000);
-		  table.addListener (SWT.SetData, new Listener () {
-		      public void handleEvent (Event event) {
-		          TableItem item = (TableItem) event.item;
-		          int index = table.indexOf (item);
-		          item.setText ("Item " + index);
-		          System.out.println (item.getText ());
-		      }
-		  });
+		final Table table = new Table (mainPanel, SWT.BORDER);
+		 TableColumn tc1 = new TableColumn(table, SWT.CENTER);
+		    TableColumn tc2 = new TableColumn(table, SWT.CENTER);
+		    TableColumn tc3 = new TableColumn(table, SWT.CENTER);
+		    tc1.setText("Class Name");
+		    tc2.setText("Attributes");
+		    tc3.setText("Methods");
+		    tc1.setWidth(100);
+		    tc2.setWidth(100);
+		    tc3.setWidth(100);
+		    table.setHeaderVisible(true);
+		    
+		    TableItem item1 = new TableItem(table, SWT.NONE);
+		    item1.setText(new String[] { "Tim", "Hatton", "Kentucky" });
+		    TableItem item2 = new TableItem(table, SWT.NONE);
+		    item2.setText(new String[] { "Caitlyn", "Warner", "Ohio" });
+		    TableItem item3 = new TableItem(table, SWT.NONE);
+		    item3.setText(new String[] { "Reese", "Miller", "Ohio" });
 		
 	}
 	
