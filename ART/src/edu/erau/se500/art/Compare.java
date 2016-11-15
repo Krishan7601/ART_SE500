fb73d8ce03199bd12978f17e6f770265ef4a099c
Krishan Patel <krishan7603@gmail.com>
2016-11-14 19:28:39 -0500

    Matched and Unmatched results stored in ArrayList of ExtractedClasses

--- ART/src/edu/erau/se500/art/Compare.java
+++ ART/src/edu/erau/se500/art/Compare.java
@@ -7,29 +7,38 @@ public class Compare {
 
 	static List<ExtractedClass> UMLClasses = new ArrayList<ExtractedClass>();
 	static List<ExtractedClass> javaClasses = new ArrayList<ExtractedClass>();
-	
-	static List<ExtractedClass> matchedClasses = new ArrayList<ExtractedClass>();
 
 	static List<CompareResults> results = new ArrayList<CompareResults>();
+
+	// parameter counts unnecessary if information stored in ExtractedClasses
+	static int parameterCount;
+	static int parameterMatch;
 
 	public static void compare(boolean doForward) {
 		if (doForward) compareForwards();
 		else compareBackwards();
 	}
 
-	private static void compareForwards() {
+	private static void compareForwards() {		
+
 		for (ExtractedClass umlClass : UMLClasses) {
 			CompareResults thisResult = new CompareResults(umlClass.name);
 			results.add(thisResult);
+			ExtractedClass currentClassUnmatch = umlClass;
+
 			for (ExtractedClass javaClass : javaClasses) {
 
 				if (umlClass.name.equals(javaClass.name)) {
+
 					thisResult.matchFound = true;
-					ExtractedClass currentMatchedClass = new ExtractedClass(umlClass.name);
+					ExtractedClass currentClassMatch = new ExtractedClass(umlClass.name);
+					currentClassUnmatch.name = null;
 
 					if (umlClass.parentClass.equals(javaClass.parentClass)) {
+
 						thisResult.parentClassMatchFound = true;
-						currentMatchedClass.parentClass = umlClass.parentClass;
+						currentClassMatch.parentClass = umlClass.parentClass;
+						currentClassUnmatch.parentClass = null;
 					}
 
 					for (String umlInterfaceClass : umlClass.interfaceClasses) {
@@ -37,8 +46,10 @@ public class Compare {
 						for (String javaInterfaceClass : javaClass.interfaceClasses) {
 
 							if (umlInterfaceClass.equals(javaInterfaceClass)) {
+
 								thisResult.interfaceClassFound++;
-								currentMatchedClass.interfaceClasses.add(umlInterfaceClass);
+								currentClassMatch.interfaceClasses.add(umlInterfaceClass);
+								currentClassMatch.interfaceClasses.remove(umlInterfaceClass);
 								break;
 							}
 						}
@@ -48,47 +59,68 @@ public class Compare {
 						thisResult.attributesTotal++;
 						for (ExtractedAttribute javaAttribute : javaClass.attributes) {
 
+
 							if (umlAttribute.name.toLowerCase().equals(javaAttribute.name.toLowerCase()) && 
 									umlAttribute.type.toLowerCase().equals(javaAttribute.type.toLowerCase())) {
+
 								thisResult.attributesFound++;
-								currentMatchedClass.attributes.add(umlAttribute);
+								currentClassMatch.attributes.add(umlAttribute);
+								currentClassUnmatch.attributes.remove(umlAttribute);
+
 								if (umlAttribute.accessModifier.equals(javaAttribute.accessModifier)) {
+
 									thisResult.accessModifiersFound++;
+									currentClassMatch.attributes.get(currentClassMatch.
+											attributes.size()).accessModifier = umlAttribute.accessModifier;
+									// remove this access modifier from currentClassUnmatch
 								}
 
 								if ((umlAttribute.isAbstract == javaAttribute.isAbstract) && 
 										(umlAttribute.isFinal == javaAttribute.isFinal) && 
 										(umlAttribute.isStatic == javaAttribute.isStatic)) {
-									
+
 									thisResult.nonAccessModifiersFound++;									
 								}
-
 								break;
 							}
 						}
 					}
 
 					for (ExtractedMethod umlMethod : umlClass.methods) {
+						parameterCount = 0;
+						parameterMatch = 0;
 						thisResult.methodsTotal++;
 						for (ExtractedMethod javaMethod : javaClass.methods) {
 
 							if (umlMethod.name.toLowerCase().equals(javaMethod.name.toLowerCase()) && 
 									umlMethod.type.toLowerCase().equals(javaMethod.type.toLowerCase())) {
+
 								thisResult.methodsFound++;
-								currentMatchedClass.methods.add(umlMethod);
-								
+								thisResult.methodNames.add(umlMethod.name);
+								currentClassMatch.methods.add(umlMethod);
+								currentClassUnmatch.methods.remove(umlMethod);
+
 								for (String umlParameter : umlMethod.parameters) {
+									parameterCount++;
 									for (String javaParameter : javaMethod.parameters) {
 
 										if (umlParameter.equals(javaParameter)) {
+
+											parameterMatch++;
+											currentClassMatch.parameters.add(umlParameter);
+											currentClassUnmatch.parameters.remove(umlParameter);
 											break;
 										}
 									}
 								}
+								thisResult.methodParameterTotal.add(parameterCount);
+								thisResult.methodParameterFound.add(parameterMatch);
 								break;
 							}
 						}
 					}
+					thisResult.matchedClasses.add(currentClassMatch);
+					thisResult.unmatchedClasses.add(currentClassUnmatch);
 					break;
 				}
 			}
--- ART/src/edu/erau/se500/art/CompareResults.java
+++ ART/src/edu/erau/se500/art/CompareResults.java
@@ -6,30 +6,30 @@ public class CompareResults {
 
 	String className;	
 	boolean matchFound = false;
-	
+
 	boolean parentClassMatchFound = false;
-	
+
 	int interfaceClassTotal = 0;
 	int interfaceClassFound = 0;
-	
+
 	int attributesTotal = 0;
 	int attributesFound = 0;
-	
+
 	int accessModifiersFound = 0;
-	
+
 	int nonAccessModifiersFound = 0;
-	
+
 	int methodsTotal = 0;
 	int methodsFound = 0;
 	
-	ArrayList<ExtractedAttribute> matchedAttribute;
+	ArrayList<ExtractedClass> matchedClasses;
+	ArrayList<ExtractedClass> unmatchedClasses;
 	
-	ArrayList<ExtractedMethod> matchedMethods;
-	ArrayList<ExtractedMethod> unmatchedMethods;
+	ArrayList<String> methodNames;
+	ArrayList<Integer> methodParameterTotal;
+	ArrayList<Integer> methodParameterFound;
+	
 
-	ArrayList<String> methodParameters;
-	ArrayList<Integer> methodParametersTotal;
-	
 	CompareResults(String className) {
 		this.className = className;
 	}
